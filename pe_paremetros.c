06a5138de0c29bcb217fe695899c98da4196e763
Isabinotti <142895514+Isabinotti@users.noreply.github.com>
2023-09-19 08:32:50 -0300

    Add files via upload

---
+++ pe_paremetros.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+float main ()
+{
+    float P,M;
+    printf("Insira a distancia em pes: ");
+    scanf("%f" , &P);
+    M= P * 0.3048;
+    printf("A distancia em metros de: %f", M);
+
+}
+

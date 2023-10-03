61524e317828c17db2b13703e24c3e059b5de9da
Isabinotti <142895514+Isabinotti@users.noreply.github.com>
2023-10-03 19:33:34 -0300

    Add files via upload

---
+++ p143.ex30.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    int x;
+    scanf("%i",&x);
+    if (x % 2 == 0)
+    {
+        printf("O numero e par");
+    }
+    else
+    {
+        printf("O numero e impar");
+    }
+}

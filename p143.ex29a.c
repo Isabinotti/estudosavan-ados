8c6255ff9c9aa353498b2ba20f1b6a561fcca895
Isabinotti <142895514+Isabinotti@users.noreply.github.com>
2023-10-03 07:41:05 -0300

    Add files via upload

---
+++ p143.ex29a.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main ()
+{
+    float I,M;
+
+    printf("digite sua idade:" );
+    scanf("%f", &I);
+    if(I>=18)
+        {
+            printf("O usuario e maior de idade");
+        }
+        else
+        {
+            printf("O usuario e menor de idade");
+        }
+    }
